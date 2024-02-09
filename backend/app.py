651c875bd7476f5f270158a5d24d57325ae941b6
madhav <madhavasok@gmail.com>
2024-02-09 11:00:54 -0800

    Cors installation and controller endpoints re-organized

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,12 @@
 from flask import Flask, Response, jsonify
 from backend.src.controllers.openai_controller import api_blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello_world():
-    return Response("Hello", status=200, headers={"Content-Type": "text/plain"})
-
-
 app.register_blueprint(api_blueprint, url_prefix='/api')
 
+CORS(app)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- backend/src/controllers/openai_controller.py
+++ backend/src/controllers/openai_controller.py
@@ -1,39 +1,57 @@
 from flask import Flask, request, jsonify, Blueprint
-
+import requests
 from backend.src.services.openai_manager import OpenAIManager
 
 api_blueprint = Blueprint('api', __name__)
 
+openai_object = OpenAIManager()
 
 @api_blueprint.route('/test', methods=['GET'])
 def test_endpoint():
     return jsonify({'message': 'OK'}), 200
 
+@api_blueprint.route('/generate/story', methods=['POST'])
+def generate_story_endpoint():
+    data = request.json
+    prompt = data.get('prompt')
+    response = openai_object.generate_story_from_prompt(prompt)
+    return response
 
-@api_blueprint.route('/open-ai', methods=['POST'])
-def openai_endpoint():
-    openai_object = OpenAIManager()
+@api_blueprint.route('/generate/summary', methods=['POST'])
+def generate_summary_endpoint():
+    data = request.json
+    prompt = data.get('prompt')
+    response = openai_object.generate_summary_from_story(prompt)
+    return response
+
+@api_blueprint.route('/generate/definition', methods=['POST'])
+def generate_definition_endpoint():
+    data = request.json
+    prompt = data.get('prompt')
+    response = openai_object.generate_definition_from_word(prompt)
+    return response
+
+@api_blueprint.route('/generate/image', methods=['POST'])
+def generate_image_endpoint():
+    data = request.json
+    prompt = data.get('prompt')
+    response = openai_object.generate_image_from_summary(prompt)
+    return response
+
+@api_blueprint.route('/readability', methods=['POST'])
+def readability_endpoint():
+    print("Readability called")
 
     data = request.json
+    query = data.get('text')
 
-    method = data.get('method')
-    prompt = data.get('prompt')
-    print(prompt)
 
-    # Prompt -> Story
-    if method == 'generate_story_from_prompt':
-        response = openai_object.generate_story_from_prompt(prompt)
+    url = "https://ipeirotis-readability-metrics.p.rapidapi.com/getReadabilityMetrics"
+    headers = {
+        "X-RapidAPI-Key": "fd784468c5mshc9a79297a50ca09p104cfcjsn026ab39a4879",
+        "X-RapidAPI-Host": "ipeirotis-readability-metrics.p.rapidapi.com"
+    }
+    response = requests.post(url, headers=headers, params=query)
 
-    # Story -> Summary
-    elif method == 'generate_summary_from_story':
-        response = openai_object.generate_summary_from_story(prompt)
+    return response.json()
 
-    # Definition -> Word
-    elif method == 'generate_definition_from_word':
-        response = openai_object.generate_summary_from_story(prompt)
-    elif method == 'generate_image_from_summary':
-        response = openai_object.generate_image_from_summary(prompt)
-
-    else:
-        return jsonify({'error': 'Method not found'}), 404
-    return response
